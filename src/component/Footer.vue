338972d663ad5ac8d1bbe960a4cc19bfc1e3bc22
LinearLaw <m18720991459_1@163.com>
2017-05-21 15:10:17 +0800

    修正部分

--- src/component/Footer.vue
+++ src/component/Footer.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <h2>This is footer!</h2>
+        <router-link to="xxx">.....</router-link>
+
+
+        <router-view></router-view>
     </div>
 </template>
 <style lang="less" scoped>
