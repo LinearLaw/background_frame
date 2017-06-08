abd5bccdd51c3bc076caa9521532b0ff35a709fc
LinearLaw <m18720991459_1@163.com>
2017-06-08 12:41:56 +0800

    修改提示

--- src/component/Login.vue
+++ src/component/Login.vue
@@ -12,7 +12,7 @@
                 <div class="login-btn">
                     <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
                 </div>
-                <p style="font-size:12px;line-height:30px;color:#999;">Tips : 用户名和密码随便填。</p>
+                <p style="font-size:12px;line-height:30px;color:#999;">Tips : 用户名和密码都是admin。</p>
             </el-form>
         </div>
     </div>
@@ -23,9 +23,6 @@
     export default {
         data: function(){
             return {
-                /**
-                 * @desc login页面没做跳转。。。
-                 */
                 ruleForm: {
                     username: '',
                     password: ''
